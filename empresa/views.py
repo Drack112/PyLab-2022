7bba523d3eeff1b7e52ae46798c60b1149b23912
Drack112 <onepessoa3@gmail.com>
2022-11-12 14:28:11 -0300

    ⚡ General Update:  Add vagas function

--- empresa/views.py
+++ empresa/views.py
@@ -2,14 +2,26 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib import messages
 from django.contrib.messages import constants
 
-from empresa.models import Empresa, Tecnologias
+from empresa.models import Empresa, Tecnologias, Vagas
 
 # Create your views here.
 
 
 def empresa(request, id):
-    empresa_unica = get_object_or_404(Empresa, id=id)
-    return render(request, "empresa_unica.html", {"empresa": empresa_unica})
+    empresa = get_object_or_404(Empresa, id=id)
+    empresas = Empresa.objects.all()
+    tecnologias = Tecnologias.objects.all()
+    vagas = Vagas.objects.filter(empresa_id=id)
+    return render(
+        request,
+        "empresa.html",
+        {
+            "empresa": empresa,
+            "tecnologias": tecnologias,
+            "empresas": empresas,
+            "vagas": vagas,
+        },
+    )
 
 
 def empresas(request):
@@ -89,16 +101,6 @@ def nova_empresa(request):
             request, constants.SUCCESS, "Empresa cadastrada com sucesso"
         )
         return redirect("/home/empresas")
-
-
-def empresas(request):
-    empresas = Empresa.objects.all()
-    tecnologias = Tecnologias.objects.all()
-    return render(
-        request,
-        "empresas.html",
-        {"empresas": empresas, "tecnologias": tecnologias},
-    )
 
 
 def excluir_empresa(request, id):
