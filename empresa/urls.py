968580c1b4b4ac5dbe8b453998fc819beb1d0126
Drack112 <onepessoa3@gmail.com>
2022-11-12 11:50:32 -0300

    ⚡ General Update:  Empresa Page

--- empresa/urls.py
+++ empresa/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from empresa.views import nova_empresa
+from empresa.views import nova_empresa, empresas
 
-urlpatterns = [path("nova_empresa/", nova_empresa, name="nova_empresa")]
+urlpatterns = [
+    path("nova_empresa/", nova_empresa, name="nova_empresa"),
+    path("empresas/", empresas, name="empresas"),
+]
--- empresa/views.py
+++ empresa/views.py
@@ -65,3 +65,13 @@ def nova_empresa(request):
             request, constants.SUCCESS, "Empresa cadastrada com sucesso"
         )
         return redirect("/home/empresas")
+
+
+def empresas(request):
+    empresas = Empresa.objects.all()
+    tecnologias = Tecnologias.objects.all()
+    return render(
+        request,
+        "empresa.html",
+        {"empresas": empresas, "tecnologias": tecnologias},
+    )
