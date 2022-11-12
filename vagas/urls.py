7a58548c952bb9557cfa5527ce2136cfa4d5a13b
Drack112 <onepessoa3@gmail.com>
2022-11-12 16:12:39 -0300

    Finish Empresa page

--- vagas/urls.py
+++ vagas/urls.py
@@ -1,5 +1,21 @@
 from django.urls import path
 
-from vagas.views import nova_vaga
+from vagas.views import (
+    nova_vaga,
+    vaga,
+    nova_tarefa,
+    realizar_tarefa,
+    envia_email,
+)
 
-urlpatterns = [path("nova_vaga/", nova_vaga, name="nova_vaga")]
+urlpatterns = [
+    path("nova_vaga/", nova_vaga, name="nova_vaga"),
+    path("vaga/<int:id>", vaga, name="vaga"),
+    path("nova_tarefa/<int:id_vaga>", nova_tarefa, name="nova_tarefa"),
+    path(
+        "realizar_tarefa/<int:id>",
+        realizar_tarefa,
+        name="realizar_tarefa",
+    ),
+    path("envia_email/<int:id_vaga>", envia_email, name="envia_email"),
+]
--- vagas/views.py
+++ vagas/views.py
@@ -1,9 +1,14 @@
-from django.http import Http404
-from django.shortcuts import redirect
+from django.conf import settings
 from django.contrib import messages
 from django.contrib.messages import constants
+from django.core.mail import EmailMultiAlternatives
+from django.http import Http404
+from django.shortcuts import get_object_or_404, redirect, render
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
 
 from empresa.models import Vagas
+from vagas.models import Emails, Tarefa
 
 
 # Create your views here.
@@ -41,3 +46,81 @@ def nova_vaga(request):
         return redirect(f"/home/empresa/{empresa}")
     elif request.method == "GET":
         raise Http404()
+
+
+def vaga(request, id):
+    vaga = get_object_or_404(Vagas, id=id)
+    emails = Emails.objects.filter(vaga=vaga)
+    tarefas = Tarefa.objects.filter(vaga=vaga).filter(realizada=False)
+    return render(
+        request,
+        "vaga.html",
+        {"vaga": vaga, "tarefas": tarefas, "emails": emails},
+    )
+
+
+def nova_tarefa(request, id_vaga):
+    titulo = request.POST.get("titulo")
+    prioridade = request.POST.get("prioridade")
+    data = request.POST.get("data")
+
+    tarefa = Tarefa(
+        vaga_id=id_vaga, titulo=titulo, prioridade=prioridade, data=data
+    )
+    tarefa.save()
+    messages.add_message(
+        request, constants.SUCCESS, "Tarefa criada com sucesso"
+    )
+    return redirect(f"/vagas/vaga/{id_vaga}")
+
+
+def realizar_tarefa(request, id):
+    tarefas_list = Tarefa.objects.filter(id=id).filter(realizada=False)
+
+    if not tarefas_list.exists():
+        messages.add_message(
+            request, constants.ERROR, "Erro interno do sistema!"
+        )
+        return redirect("/home/empresas/")
+
+    tarefa = tarefas_list.first()
+    tarefa.realizada = True
+    tarefa.save()
+    messages.add_message(
+        request, constants.SUCCESS, "Tarefa realizada com sucesso, parabéns!"
+    )
+    return redirect(f"/vagas/vaga/{tarefa.vaga.id}")
+
+
+def envia_email(request, id_vaga):
+    vaga = Vagas.objects.get(id=id_vaga)
+    assunto = request.POST.get("assunto")
+    corpo = request.POST.get("corpo")
+
+    html_content = render_to_string(
+        "emails/template_email.html", {"corpo": corpo}
+    )
+    text_content = strip_tags(html_content)
+    email = EmailMultiAlternatives(
+        assunto,
+        text_content,
+        settings.EMAIL_HOST_USER,
+        [
+            vaga.email,
+        ],
+    )
+    email.attach_alternative(html_content, "text/html")
+    if email.send():
+        mail = Emails(vaga=vaga, assunto=assunto, corpo=corpo, enviado=True)
+        mail.save()
+        messages.add_message(
+            request, constants.SUCCESS, "Email enviado com sucesso."
+        )
+        return redirect(f"/vagas/vaga/{id_vaga}")
+    else:
+        mail = Emails(vaga=vaga, assunto=assunto, corpo=corpo, enviado=False)
+        mail.save()
+        messages.add_message(
+            request, constants.ERROR, "Erro interno do sistema!"
+        )
+        return redirect(f"/vagas/vaga/{id_vaga}")
